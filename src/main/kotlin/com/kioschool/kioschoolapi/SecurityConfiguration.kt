de581d8f0e2492b7ee8bef7b7abc4b40450403b3
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2023-11-14 00:02:36 +0900

    Create SecurityConfiguration.kt for accept all request while develop

---
+++ src/main/kotlin/com/kioschool/kioschoolapi/SecurityConfiguration.kt
@@ -0,0 +1,21 @@
+package com.kioschool.kioschoolapi
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.DefaultSecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfiguration {
+
+    @Bean
+    fun filterChain(httpSecurity: HttpSecurity): DefaultSecurityFilterChain? {
+        httpSecurity
+            .csrf { it.disable() }
+            .authorizeHttpRequests { it.requestMatchers("/**").permitAll() }
+
+        return httpSecurity.build()
+    }
+}
