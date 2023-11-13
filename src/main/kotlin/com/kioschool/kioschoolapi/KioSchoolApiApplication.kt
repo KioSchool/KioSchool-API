f5ded9b05e39c7ccc1d598c405dc6d868dc4c4c7
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2023-11-14 00:03:18 +0900

    Add auditting annotation

--- src/main/kotlin/com/kioschool/kioschoolapi/KioSchoolApiApplication.kt
+++ src/main/kotlin/com/kioschool/kioschoolapi/KioSchoolApiApplication.kt
@@ -2,8 +2,10 @@ package com.kioschool.kioschoolapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class KioSchoolApiApplication
 
 fun main(args: Array<String>) {
