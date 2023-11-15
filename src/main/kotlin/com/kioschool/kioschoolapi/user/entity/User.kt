1cbe35f96b32de83e22a84244a76d1675a6bb40b
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2023-11-16 01:32:59 +0900

    Add table info to User entity

--- src/main/kotlin/com/kioschool/kioschoolapi/user/entity/User.kt
+++ src/main/kotlin/com/kioschool/kioschoolapi/user/entity/User.kt
@@ -3,8 +3,10 @@ package com.kioschool.kioschoolapi.user.entity
 import com.kioschool.kioschoolapi.common.entity.BaseEntity
 import com.kioschool.kioschoolapi.common.enums.UserRole
 import jakarta.persistence.Entity
+import jakarta.persistence.Table
 
 @Entity
+@Table(name = "user", schema = "PUBLIC")
 class User(
     var loginId: String,
     var loginPassword: String,
