69ca4c7ac42c708379eae89f2611b8ab85195b86
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2023-11-16 01:33:49 +0900

    Add test login endpoint for develop

--- src/main/kotlin/com/kioschool/kioschoolapi/user/controller/UserController.kt
+++ src/main/kotlin/com/kioschool/kioschoolapi/user/controller/UserController.kt
@@ -1,9 +1,15 @@
 package com.kioschool.kioschoolapi.user.controller
 
 import com.kioschool.kioschoolapi.user.service.UserService
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class UserController(
     private val userService: UserService
-)
+) {
+    @PostMapping("/login")
+    fun login() {
+        userService.login("test", "test")
+    }
+}
--- src/main/kotlin/com/kioschool/kioschoolapi/user/service/UserService.kt
+++ src/main/kotlin/com/kioschool/kioschoolapi/user/service/UserService.kt
@@ -1,9 +1,17 @@
 package com.kioschool.kioschoolapi.user.service
 
+import com.kioschool.kioschoolapi.security.JwtProvider
 import com.kioschool.kioschoolapi.user.repository.UserRepository
 import org.springframework.stereotype.Service
 
 @Service
 class UserService(
-    private val userRepository: UserRepository
-)
+    private val userRepository: UserRepository,
+    private val jwtProvider: JwtProvider
+) {
+    fun login(loginId: String, loginPassword: String) {
+        val user = userRepository.findByLoginId(loginId)
+        val token = jwtProvider.createToken(user)
+        println(token)
+    }
+}
