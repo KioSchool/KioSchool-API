727829badbd17349f3482edbea760893207568a2
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2023-11-19 00:08:39 +0900

    Fix exception

--- src/main/kotlin/com/kioschool/kioschoolapi/security/CustomUserDetailService.kt
+++ src/main/kotlin/com/kioschool/kioschoolapi/security/CustomUserDetailService.kt
@@ -1,6 +1,6 @@
 package com.kioschool.kioschoolapi.security
 
-import com.kioschool.kioschoolapi.user.exception.LoginFailedException
+import com.kioschool.kioschoolapi.user.exception.InvalidJwtException
 import com.kioschool.kioschoolapi.user.repository.UserRepository
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
@@ -11,7 +11,7 @@ class CustomUserDetailService(
     val userRepository: UserRepository
 ) : UserDetailsService {
     override fun loadUserByUsername(username: String?): UserDetails {
-        val user = userRepository.findByLoginId(username!!) ?: throw LoginFailedException()
+        val user = userRepository.findByLoginId(username!!) ?: throw InvalidJwtException()
         return CustomUserDetails(user)
     }
 }
