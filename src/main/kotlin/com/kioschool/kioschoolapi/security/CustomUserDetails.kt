5c1461c717e7b8403d7695cda9931fed474b8834
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2023-11-18 10:32:44 +0900

    Edit some code for implement jwt

--- src/main/kotlin/com/kioschool/kioschoolapi/security/CustomUserDetails.kt
+++ src/main/kotlin/com/kioschool/kioschoolapi/security/CustomUserDetails.kt
@@ -2,13 +2,14 @@ package com.kioschool.kioschoolapi.security
 
 import com.kioschool.kioschoolapi.user.entity.User
 import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
 
 class CustomUserDetails(
     private val user: User
 ) : UserDetails {
     override fun getAuthorities(): MutableCollection<out GrantedAuthority> =
-        mutableListOf(GrantedAuthority { user.role.name })
+        mutableListOf(SimpleGrantedAuthority(user.role.name))
 
     override fun getPassword(): String = user.loginPassword
 
--- src/main/kotlin/com/kioschool/kioschoolapi/security/JwtProvider.kt
+++ src/main/kotlin/com/kioschool/kioschoolapi/security/JwtProvider.kt
@@ -5,23 +5,64 @@ import com.nimbusds.jose.util.StandardCharset
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
 import io.jsonwebtoken.security.Keys
+import jakarta.servlet.http.HttpServletRequest
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.Authentication
 import org.springframework.stereotype.Component
+import java.util.*
 
 
 @Component
 class JwtProvider(
     @Value("\${jwt.secret-key}")
-    private val salt: String
+    private val salt: String,
+    private val userDetailService: CustomUserDetailService
 ) {
     private val secretKey = Keys.hmacShaKeyFor(salt.toByteArray(StandardCharset.UTF_8))
+    private val expirationTime = 1000L * 60 * 60 * 24 * 7
 
     fun createToken(user: User): String {
         val claims = Jwts.claims().setSubject(user.loginId)
-        claims["role"] = user.role
+        val now = Date()
+        claims["roles"] = listOf(user.role.name)
         return Jwts.builder()
+            .setIssuedAt(Date())
             .setClaims(claims)
+            .setExpiration(Date(now.time + expirationTime))
             .signWith(secretKey, SignatureAlgorithm.HS256)
             .compact()
+    }
+
+    fun resolveToken(request: HttpServletRequest): String? {
+        val rawToken = request.getHeader("Authorization") ?: return null
+
+        return rawToken.replace("Bearer ", "")
+    }
+
+    fun validateToken(token: String): Boolean {
+        return try {
+            val claims = Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+            return claims.body.expiration.after(Date())
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    fun getAuthentication(token: String): Authentication {
+        val userDetails = userDetailService.loadUserByUsername(getLoginId(token))
+        return UsernamePasswordAuthenticationToken(userDetails, "", userDetails.authorities)
+    }
+
+    fun getLoginId(token: String): String {
+        return Jwts.parserBuilder()
+            .setSigningKey(secretKey)
+            .build()
+            .parseClaimsJws(token)
+            .body
+            .subject
     }
 }
