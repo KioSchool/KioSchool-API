2ddd27b986850278b8b88332c749f44a21ac3091
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2023-12-07 00:46:59 +0900

    Add websocket dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,6 +36,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-mail")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-websocket")
     implementation("io.awspring.cloud:spring-cloud-starter-aws:2.4.2")
     implementation("com.amazonaws:aws-java-sdk-s3:1.12.595")
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
