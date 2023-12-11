8f05561c822f3303d2c88361a1c0d9a8f491c0d6
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2023-12-11 17:42:36 +0900

    Add cors

---
+++ src/main/kotlin/com/kioschool/kioschoolapi/WebMvcConfiguration.kt
@@ -0,0 +1,17 @@
+package com.kioschool.kioschoolapi
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebMvcConfiguration : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry
+            .addMapping("/**")
+            .allowedHeaders("*")
+            .allowedOrigins("http://localhost:3000", "https://kio-school.com")
+            .allowedMethods("*")
+            .allowCredentials(true)
+    }
+}
