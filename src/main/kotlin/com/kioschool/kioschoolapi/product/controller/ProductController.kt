7fa9c53401d2091175962543fb5c1966a73e4a57
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2023-12-14 23:25:07 +0900

    Edit user get product endpoint

--- src/main/kotlin/com/kioschool/kioschoolapi/product/controller/ProductController.kt
+++ src/main/kotlin/com/kioschool/kioschoolapi/product/controller/ProductController.kt
@@ -1,22 +1,21 @@
 package com.kioschool.kioschoolapi.product.controller
 
-import com.kioschool.kioschoolapi.product.dto.GetProductsRequestBody
 import com.kioschool.kioschoolapi.product.service.ProductService
 import com.kioschool.kioschoolapi.user.dto.ExceptionResponseBody
 import com.kioschool.kioschoolapi.workspace.exception.WorkspaceInaccessibleException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class ProductController(
     private val productService: ProductService
 ) {
-    @GetMapping("/products")
+    @GetMapping("/{workspaceId}/products")
     fun getProducts(
-        @RequestBody body: GetProductsRequestBody
-    ) = productService.getProducts(body.workspaceId)
+        @PathVariable workspaceId: Long
+    ) = productService.getProducts(workspaceId)
 
     @ExceptionHandler(
         WorkspaceInaccessibleException::class,
