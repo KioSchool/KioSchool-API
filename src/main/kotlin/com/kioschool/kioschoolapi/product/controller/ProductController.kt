5cd6c23e47e425d83e590b94ca46627ea5f22d69
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2023-12-22 23:05:47 +0900

    Add swagger description to product controller

--- src/main/kotlin/com/kioschool/kioschoolapi/product/controller/ProductController.kt
+++ src/main/kotlin/com/kioschool/kioschoolapi/product/controller/ProductController.kt
@@ -3,15 +3,19 @@ package com.kioschool.kioschoolapi.product.controller
 import com.kioschool.kioschoolapi.product.service.ProductService
 import com.kioschool.kioschoolapi.user.dto.ExceptionResponseBody
 import com.kioschool.kioschoolapi.workspace.exception.WorkspaceInaccessibleException
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
+@Tag(name = "Product Controller")
 @RestController
 class ProductController(
     private val productService: ProductService
 ) {
+    @Operation(summary = "상품 조회", description = "워크스페이스에 등록된 모든 상품을 조회합니다.")
     @GetMapping("/products")
     fun getProducts(
         @RequestParam workspaceId: Long
