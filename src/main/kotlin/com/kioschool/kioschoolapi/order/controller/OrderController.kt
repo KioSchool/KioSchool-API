e4dbcaddd1484ecb00ee28c37748c5849b8b12f6
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2023-12-22 22:59:46 +0900

    Add swagger description to order controller

--- src/main/kotlin/com/kioschool/kioschoolapi/order/controller/OrderController.kt
+++ src/main/kotlin/com/kioschool/kioschoolapi/order/controller/OrderController.kt
@@ -1,15 +1,19 @@
 package com.kioschool.kioschoolapi.order.controller
 
 import com.kioschool.kioschoolapi.order.dto.CreateOrderRequestBody
-import com.kioschool.kioschoolapi.order.dto.GetOrdersByPhoneNumberRequestBody
 import com.kioschool.kioschoolapi.order.entity.Order
 import com.kioschool.kioschoolapi.order.service.OrderService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.*
 
+@Tag(name = "Order Controller")
 @RestController
 class OrderController(
     private val orderService: OrderService
 ) {
+
+    @Operation(summary = "주문 생성", description = "주문을 생성합니다.")
     @PostMapping("/order")
     fun createOrder(
         @RequestBody body: CreateOrderRequestBody
@@ -22,6 +26,7 @@ class OrderController(
         )
     }
 
+    @Operation(summary = "전화번호로 주문 조회", description = "전화번호로 주문을 조회합니다.")
     @GetMapping("/order")
     fun getOrdersByPhoneNumber(
         @RequestParam phoneNumber: String,
