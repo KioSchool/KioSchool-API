3cf834967d5c84616dc225e5f031c59134bd9e99
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2023-12-22 22:55:26 +0900

    Add swagger description to user controller

--- src/main/kotlin/com/kioschool/kioschoolapi/user/controller/UserController.kt
+++ src/main/kotlin/com/kioschool/kioschoolapi/user/controller/UserController.kt
@@ -6,17 +6,21 @@ import com.kioschool.kioschoolapi.user.dto.*
 import com.kioschool.kioschoolapi.user.exception.LoginFailedException
 import com.kioschool.kioschoolapi.user.exception.RegisterException
 import com.kioschool.kioschoolapi.user.service.UserService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.http.HttpHeaders
 import org.springframework.http.ResponseCookie
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
+@Tag(name = "User Controller")
 @RestController
 class UserController(
     private val userService: UserService,
     private val emailService: EmailService
 ) {
+    @Operation(summary = "로그인", description = "로그인 성공 시 쿠키에 JWT 토큰을 담아 반환합니다.")
     @PostMapping("/login")
     @ResponseBody
     fun login(
@@ -34,6 +38,10 @@ class UserController(
         return ResponseEntity.ok().body("login success")
     }
 
+    @Operation(
+        summary = "회원가입",
+        description = "이메일 인증이 되어있어야지만 회원가입에 성공합니다.<br>회원가입 성공 시 쿠키에 JWT 토큰을 담아 반환합니다."
+    )
     @PostMapping("/register")
     @ResponseBody
     fun register(
@@ -54,16 +62,19 @@ class UserController(
         return ResponseEntity.ok().body("register success")
     }
 
+    @Operation(summary = "ID 중복 체크", description = "중복되는 ID가 있으면 true를 반환합니다.")
     @PostMapping("/user/duplicate")
     fun isDuplicateLoginId(@RequestBody body: IsDuplicateLoginIdRequestBody): Boolean {
         return userService.isDuplicateLoginId(body.id)
     }
 
+    @Operation(summary = "이메일 인증코드 발송", description = "이메일 인증코드를 발송합니다.")
     @PostMapping("/user/email")
     fun sendEmailCode(@RequestBody body: SendEmailCodeRequestBody) {
         return emailService.sendRegisterCodeEmail(body.email)
     }
 
+    @Operation(summary = "이메일 인증코드 확인", description = "이메일 인증코드를 확인합니다.")
     @PostMapping("/user/verify")
     fun verifyEmailCode(@RequestBody body: VerifyEmailCodeRequestBody): Boolean {
         return emailService.verifyRegisterCode(body.email, body.code)
