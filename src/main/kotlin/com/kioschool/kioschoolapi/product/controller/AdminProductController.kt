9c8bcf09153ab25dc2bfb357cfe84b09cf07fd61
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2023-12-22 23:07:16 +0900

    Add swagger description to admin product controller

--- src/main/kotlin/com/kioschool/kioschoolapi/product/controller/AdminProductController.kt
+++ src/main/kotlin/com/kioschool/kioschoolapi/product/controller/AdminProductController.kt
@@ -6,21 +6,26 @@ import com.kioschool.kioschoolapi.product.service.ProductService
 import com.kioschool.kioschoolapi.security.CustomUserDetails
 import com.kioschool.kioschoolapi.user.dto.ExceptionResponseBody
 import com.kioschool.kioschoolapi.workspace.exception.WorkspaceInaccessibleException
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.MediaType
 import org.springframework.security.core.Authentication
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 
+@Tag(name = "Admin Product Controller")
 @RestController
 @RequestMapping("/admin")
 class AdminProductController(
     private val productService: ProductService
 ) {
+    @Operation(summary = "상품 조회", description = "워크스페이스에 등록된 모든 상품을 조회합니다.")
     @GetMapping("/products")
     fun getProducts(
         @RequestParam workspaceId: Long
     ) = productService.getProducts(workspaceId)
 
+    @Operation(summary = "상품 생성/수정", description = "상품을 생성/수정합니다.<br>상품 ID가 있으면 수정, 없으면 생성합니다.")
     @PostMapping("/product", consumes = [MediaType.ALL_VALUE])
     fun createOrUpdateProduct(
         authentication: Authentication,
