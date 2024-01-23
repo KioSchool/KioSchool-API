9f238104f58746e0006368181146aad9de13349e
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2024-01-23 23:17:22 +0900

    Add workspace id to product category

--- src/main/kotlin/com/kioschool/kioschoolapi/product/entity/ProductCategory.kt
+++ src/main/kotlin/com/kioschool/kioschoolapi/product/entity/ProductCategory.kt
@@ -1,11 +1,17 @@
 package com.kioschool.kioschoolapi.product.entity
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import com.kioschool.kioschoolapi.common.entity.BaseEntity
+import com.kioschool.kioschoolapi.workspace.entity.Workspace
 import jakarta.persistence.Entity
+import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
 
 @Entity
 @Table(name = "product_category")
 class ProductCategory(
-    var name: String
+    var name: String,
+    @ManyToOne
+    @JsonIgnore
+    val workspace: Workspace,
 ) : BaseEntity()
