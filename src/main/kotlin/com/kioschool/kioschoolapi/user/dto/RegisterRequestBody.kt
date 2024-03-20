bcb7c9b4af29ee2aec943c848ae3a1c21dbaf0df
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2024-03-21 02:25:09 +0900

    Add validate password min max

--- src/main/kotlin/com/kioschool/kioschoolapi/user/dto/RegisterRequestBody.kt
+++ src/main/kotlin/com/kioschool/kioschoolapi/user/dto/RegisterRequestBody.kt
@@ -2,11 +2,13 @@ package com.kioschool.kioschoolapi.user.dto
 
 import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotBlank
+import org.hibernate.validator.constraints.Length
 
 data class RegisterRequestBody(
     @field:NotBlank(message = "아이디는 필수 입력값입니다.")
     val id: String,
     @field:NotBlank(message = "비밀번호는 필수 입력값입니다.")
+    @field:Length(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하로 입력해주세요.")
     val password: String,
     @field:NotBlank(message = "이름은 필수 입력값입니다.")
     val name: String,
