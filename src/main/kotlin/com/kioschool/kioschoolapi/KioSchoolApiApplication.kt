83a4cde6056e7559eb44d263ae4a9cc189ad1cd3
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2024-05-07 18:28:54 +0900

    Add timezone

--- src/main/kotlin/com/kioschool/kioschoolapi/KioSchoolApiApplication.kt
+++ src/main/kotlin/com/kioschool/kioschoolapi/KioSchoolApiApplication.kt
@@ -1,13 +1,21 @@
 package com.kioschool.kioschoolapi
 
+import jakarta.annotation.PostConstruct
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import java.util.*
 
 @SpringBootApplication
 @EnableJpaAuditing
 class KioSchoolApiApplication
 
+@PostConstruct
+fun started() {
+    TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"))
+}
+
 fun main(args: Array<String>) {
     runApplication<KioSchoolApiApplication>(*args)
+
 }
