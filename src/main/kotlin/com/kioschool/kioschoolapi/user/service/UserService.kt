eecace47bcd291dbb9d82cc5e2a6d3ffe4e54dc3
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2024-09-09 14:29:43 +0900

    Remove useless parameter

--- src/main/kotlin/com/kioschool/kioschoolapi/user/service/UserService.kt
+++ src/main/kotlin/com/kioschool/kioschoolapi/user/service/UserService.kt
@@ -1,9 +1,7 @@
 package com.kioschool.kioschoolapi.user.service
 
 import com.kioschool.kioschoolapi.common.enums.UserRole
-import com.kioschool.kioschoolapi.discord.DiscordService
 import com.kioschool.kioschoolapi.email.service.EmailService
-import com.kioschool.kioschoolapi.security.JwtProvider
 import com.kioschool.kioschoolapi.user.entity.User
 import com.kioschool.kioschoolapi.user.exception.LoginFailedException
 import com.kioschool.kioschoolapi.user.exception.NoPermissionException
@@ -18,10 +16,8 @@ import org.springframework.stereotype.Service
 @Service
 class UserService(
     private val userRepository: UserRepository,
-    private val jwtProvider: JwtProvider,
     private val passwordEncoder: PasswordEncoder,
     private val emailService: EmailService,
-    private val discordService: DiscordService
 ) {
     fun checkPassword(user: User, loginPassword: String) {
         if (!passwordEncoder.matches(
