41c3fb0c84c06e79778fbd8b8a0b86ba3ead1c8a
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2024-09-10 16:26:22 +0900

    Edit to use super admin annotation

--- src/main/kotlin/com/kioschool/kioschoolapi/email/controller/SuperAdminEmailController.kt
+++ src/main/kotlin/com/kioschool/kioschoolapi/email/controller/SuperAdminEmailController.kt
@@ -1,16 +1,14 @@
 package com.kioschool.kioschoolapi.email.controller
 
+import com.kioschool.kioschoolapi.common.annotation.SuperAdmin
 import com.kioschool.kioschoolapi.email.dto.RegisterEmailDomainRequestBody
 import com.kioschool.kioschoolapi.email.dto.RemoveEmailDomainRequestBody
 import com.kioschool.kioschoolapi.email.entity.EmailDomain
 import com.kioschool.kioschoolapi.email.service.EmailService
-import com.kioschool.kioschoolapi.security.CustomUserDetails
-import com.kioschool.kioschoolapi.user.exception.NoPermissionException
 import com.kioschool.kioschoolapi.user.service.UserService
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.data.domain.Page
-import org.springframework.security.core.Authentication
 import org.springframework.web.bind.annotation.*
 
 @Tag(name = "Super Admin Email Controller")
@@ -23,38 +21,29 @@ class SuperAdminEmailController(
     @Operation(summary = "이메일 도메인 조회", description = "키오스쿨에서 사용 가능한 모든 이메일 도메인을 조회합니다.")
     @GetMapping("/email-domains")
     fun getWorkspaces(
-        authentication: Authentication,
+        @SuperAdmin username: String,
         @RequestParam(required = false) name: String?,
         @RequestParam page: Int,
         @RequestParam size: Int
     ): Page<EmailDomain> {
-        val username = (authentication.principal as CustomUserDetails).username
-        if (!userService.isSuperAdminUser(username)) throw NoPermissionException()
-
         return emailService.getAllEmailDomains(name, page, size)
     }
 
     @Operation(summary = "이메일 도메인 등록", description = "키오스쿨에서 사용 가능하게끔 이메일 도메인을 등록합니다.")
     @PostMapping("/email-domain")
     fun registerEmailDomain(
-        authentication: Authentication,
+        @SuperAdmin username: String,
         @RequestBody body: RegisterEmailDomainRequestBody
     ): EmailDomain {
-        val username = (authentication.principal as CustomUserDetails).username
-        if (!userService.isSuperAdminUser(username)) throw NoPermissionException()
-
         return emailService.registerEmailDomain(body.name, body.domain)
     }
 
     @Operation(summary = "이메일 도메인 삭제", description = "키오스쿨에 등록된 이메일 도메인을 삭제합니다.")
     @DeleteMapping("/email-domain")
     fun removeEmailDomain(
-        authentication: Authentication,
+        @SuperAdmin username: String,
         @RequestBody body: RemoveEmailDomainRequestBody
     ): EmailDomain {
-        val username = (authentication.principal as CustomUserDetails).username
-        if (!userService.isSuperAdminUser(username)) throw NoPermissionException()
-
         return emailService.removeEmailDomain(body.domainId)
     }
 }
