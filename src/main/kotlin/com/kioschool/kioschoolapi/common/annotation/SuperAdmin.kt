0e7563b933b053a0334a7b9fa21be87562735f13
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2024-09-13 14:14:24 +0900

    Edit hide custom annotation parameter

--- src/main/kotlin/com/kioschool/kioschoolapi/common/annotation/SuperAdmin.kt
+++ src/main/kotlin/com/kioschool/kioschoolapi/common/annotation/SuperAdmin.kt
@@ -1,5 +1,8 @@
 package com.kioschool.kioschoolapi.common.annotation
 
+import io.swagger.v3.oas.annotations.Parameter
+
 @Target(AnnotationTarget.VALUE_PARAMETER)
 @Retention(AnnotationRetention.RUNTIME)
+@Parameter(hidden = true)
 annotation class SuperAdmin
