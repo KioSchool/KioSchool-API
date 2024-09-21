f2fa7b52e124406b7f1d47503d85e0cd58ac289f
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2024-09-21 15:59:39 +0900

    Remove useless method

--- src/main/kotlin/com/kioschool/kioschoolapi/order/repository/OrderRepository.kt
+++ src/main/kotlin/com/kioschool/kioschoolapi/order/repository/OrderRepository.kt
@@ -5,6 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface OrderRepository : JpaRepository<Order, Long> {
-    fun findAllByWorkspaceId(workspaceId: Long): List<Order>
-}
+interface OrderRepository : JpaRepository<Order, Long> 
