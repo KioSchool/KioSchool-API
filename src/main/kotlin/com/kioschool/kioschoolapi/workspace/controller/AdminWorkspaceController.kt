08c2e1266e4d6376d0f2cb03f643553608531df2
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2024-09-26 15:10:08 +0900

    Refactor apply username annotation to admin workspace controller

--- src/main/kotlin/com/kioschool/kioschoolapi/workspace/controller/AdminWorkspaceController.kt
+++ src/main/kotlin/com/kioschool/kioschoolapi/workspace/controller/AdminWorkspaceController.kt
@@ -1,16 +1,11 @@
 package com.kioschool.kioschoolapi.workspace.controller
 
 import com.kioschool.kioschoolapi.common.annotation.Username
-import com.kioschool.kioschoolapi.security.CustomUserDetails
-import com.kioschool.kioschoolapi.workspace.dto.CreateWorkspaceRequestBody
-import com.kioschool.kioschoolapi.workspace.dto.InviteWorkspaceRequestBody
-import com.kioschool.kioschoolapi.workspace.dto.JoinWorkspaceRequestBody
-import com.kioschool.kioschoolapi.workspace.dto.LeaveWorkspaceRequestBody
+import com.kioschool.kioschoolapi.workspace.dto.*
 import com.kioschool.kioschoolapi.workspace.entity.Workspace
 import com.kioschool.kioschoolapi.workspace.facade.WorkspaceFacade
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
-import org.springframework.security.core.Authentication
 import org.springframework.web.bind.annotation.*
 
 @Tag(name = "Admin Workspace Controller")
@@ -37,40 +32,36 @@ class AdminWorkspaceController(
     @Operation(summary = "워크스페이스 생성", description = "워크스페이스를 생성합니다.")
     @PostMapping("/workspace")
     fun createWorkspace(
-        authentication: Authentication,
+        @Username username: String,
         @RequestBody body: CreateWorkspaceRequestBody
     ): Workspace {
-        val username = (authentication.principal as CustomUserDetails).username
         return workspaceFacade.createWorkspace(username, body.name, body.description)
     }
 
     @Operation(summary = "워크스페이스 초대", description = "워크스페이스에 사용자를 초대합니다.")
     @PostMapping("/workspace/invite")
     fun inviteWorkspace(
-        authentication: Authentication,
+        @Username username: String,
         @RequestBody body: InviteWorkspaceRequestBody
     ): Workspace {
-        val username = (authentication.principal as CustomUserDetails).username
         return workspaceFacade.inviteWorkspace(username, body.workspaceId, body.userLoginId)
     }
 
     @Operation(summary = "워크스페이스 가입", description = "워크스페이스에 가입합니다.<br>초대를 받은 사용자만 가입할 수 있습니다.")
     @PostMapping("/workspace/join")
     fun joinWorkspace(
-        authentication: Authentication,
+        @Username username: String,
         @RequestBody body: JoinWorkspaceRequestBody
     ): Workspace {
-        val username = (authentication.principal as CustomUserDetails).username
         return workspaceFacade.joinWorkspace(username, body.workspaceId)
     }
 
     @Operation(summary = "워크스페이스 탈퇴", description = "워크스페이스에서 탈퇴합니다.")
     @PostMapping("/workspace/leave")
     fun leaveWorkspace(
-        authentication: Authentication,
+        @Username username: String,
         @RequestBody body: LeaveWorkspaceRequestBody
     ): Workspace {
-        val username = (authentication.principal as CustomUserDetails).username
         return workspaceFacade.leaveWorkspace(username, body.workspaceId)
     }
 }
