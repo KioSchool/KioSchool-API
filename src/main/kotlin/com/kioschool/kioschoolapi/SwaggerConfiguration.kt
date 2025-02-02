2282fd220585ebb65699bc1069ef220731567483
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2025-02-02 21:17:56 +0900

    Add swagger group

---
+++ src/main/kotlin/com/kioschool/kioschoolapi/SwaggerConfiguration.kt
@@ -0,0 +1,32 @@
+package com.kioschool.kioschoolapi
+
+import org.springdoc.core.models.GroupedOpenApi
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class SwaggerConfiguration {
+    @Bean
+    fun superAdminGroup(): GroupedOpenApi {
+        return GroupedOpenApi.builder()
+            .group("super-admin")
+            .pathsToMatch("/super-admin/**")
+            .build()
+    }
+
+    @Bean
+    fun adminGroup(): GroupedOpenApi {
+        return GroupedOpenApi.builder()
+            .group("admin")
+            .pathsToMatch("/admin/**")
+            .build()
+    }
+
+    @Bean
+    fun userGroup(): GroupedOpenApi {
+        return GroupedOpenApi.builder()
+            .group("user")
+            .pathsToExclude("/admin/**", "/super-admin/**")
+            .build()
+    }
+}
--- src/main/kotlin/com/kioschool/kioschoolapi/workspace/controller/WorkspaceController.kt
+++ src/main/kotlin/com/kioschool/kioschoolapi/workspace/controller/WorkspaceController.kt
@@ -3,10 +3,12 @@ package com.kioschool.kioschoolapi.workspace.controller
 import com.kioschool.kioschoolapi.workspace.entity.Workspace
 import com.kioschool.kioschoolapi.workspace.facade.WorkspaceFacade
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
+@Tag(name = "Workspace Controller")
 @RestController
 class WorkspaceController(
     private val workspaceFacade: WorkspaceFacade
