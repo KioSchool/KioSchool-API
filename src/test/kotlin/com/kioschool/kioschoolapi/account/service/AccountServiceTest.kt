aa92bae654ffbaba4faaf37f3c8fd097b26e765f
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2025-03-12 15:56:43 +0900

    Create AccountServiceTest.kt

---
+++ src/test/kotlin/com/kioschool/kioschoolapi/account/service/AccountServiceTest.kt
@@ -0,0 +1,35 @@
+package com.kioschool.kioschoolapi.account.service
+
+import com.kioschool.kioschoolapi.account.entity.Account
+import com.kioschool.kioschoolapi.account.repository.AccountRepository
+import com.kioschool.kioschoolapi.factory.SampleEntity
+import io.kotest.core.spec.style.DescribeSpec
+import io.mockk.*
+
+class AccountServiceTest : DescribeSpec({
+    val accountRepository = mockk<AccountRepository>()
+
+    val sut = AccountService(accountRepository)
+
+    beforeTest {
+        mockkObject(accountRepository)
+    }
+
+    afterTest {
+        clearAllMocks()
+    }
+
+    describe("createAccount") {
+        it("should call accountRepository.save") {
+            val bank = SampleEntity.bank
+
+            every { accountRepository.save(any<Account>()) } returns SampleEntity.account
+
+            val result = sut.createAccount(bank, "1234", "John Doe")
+
+            assert(result == SampleEntity.account)
+
+            verify { accountRepository.save(any()) }
+        }
+    }
+})
