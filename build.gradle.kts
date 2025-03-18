53f52e858ac208d5e2b6664f56f8c0c7ad71e3bf
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2025-03-18 16:52:19 +0900

    Add redis dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-websocket")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("io.awspring.cloud:spring-cloud-starter-aws:2.4.2")
     implementation("com.amazonaws:aws-java-sdk-s3:1.12.595")
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
