ef0cf2c7b4d078e0a107169e3fe96446b688b53d
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2025-07-29 21:45:29 +0900

    Edit NoOrderSessionException reason

--- src/main/kotlin/com/kioschool/kioschoolapi/domain/order/exception/NoOrderSessionException.kt
+++ src/main/kotlin/com/kioschool/kioschoolapi/domain/order/exception/NoOrderSessionException.kt
@@ -4,6 +4,6 @@ import org.springframework.web.bind.annotation.ResponseStatus
 
 @ResponseStatus(
     value = org.springframework.http.HttpStatus.BAD_REQUEST,
-    reason = "테이블이 사용상태가 아니라 주문을 받을 수 없습니다."
+    reason = "테이블이 사용상태가 아니라 주문을 받을 수 없습니다. 주점 관리자에게 문의하세요."
 )
 class NoOrderSessionException : Exception()
