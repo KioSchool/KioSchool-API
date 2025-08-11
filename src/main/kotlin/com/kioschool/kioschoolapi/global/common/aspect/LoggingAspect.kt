44edc08d2a31cf79b8047ad3530eabae2216c7ad
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2025-08-11 14:35:59 +0900

    Refactor move logging aspect

--- src/main/kotlin/com/kioschool/kioschoolapi/global/common/aspect/LoggingAspect.kt
+++ src/main/kotlin/com/kioschool/kioschoolapi/global/common/aspect/LoggingAspect.kt
@@ -1,4 +1,4 @@
-package com.kioschool.kioschoolapi.global.common.annotation
+package com.kioschool.kioschoolapi.global.common.aspect
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import jakarta.servlet.ServletRequest
