33f70f4910ca8330f79a9cf555ae3ad9d2b52922
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2025-10-02 00:51:30 +0900

    Edit use allowed orgins properties at cors setting

--- src/main/kotlin/com/kioschool/kioschoolapi/global/configuration/WebMvcConfiguration.kt
+++ src/main/kotlin/com/kioschool/kioschoolapi/global/configuration/WebMvcConfiguration.kt
@@ -1,6 +1,7 @@
 package com.kioschool.kioschoolapi.global.configuration
 
 import com.kioschool.kioschoolapi.global.common.resolver.AuthenticationArgumentResolver
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.servlet.config.annotation.CorsRegistry
@@ -8,13 +9,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebMvcConfiguration(
+    @Value("\${websocket.allowed-origins}")
+    private val allowedOrigins: String,
     private val authenticationArgumentResolver: AuthenticationArgumentResolver,
 ) : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry
             .addMapping("/**")
             .allowedHeaders("*")
-            .allowedOrigins("http://localhost:3000", "https://kio-school.com")
+            .allowedOrigins(allowedOrigins)
             .allowedMethods("*")
             .allowCredentials(true)
     }
