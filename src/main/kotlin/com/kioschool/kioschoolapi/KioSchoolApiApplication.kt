af68ccd20b01de2c5b378c4466a79536dbb1927b
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2026-02-17 16:17:26 +0900

    Update KioSchoolApiApplication.kt

--- src/main/kotlin/com/kioschool/kioschoolapi/KioSchoolApiApplication.kt
+++ src/main/kotlin/com/kioschool/kioschoolapi/KioSchoolApiApplication.kt
@@ -6,6 +6,9 @@ import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.boot.CommandLineRunner
+import org.springframework.context.ApplicationContext
+import org.springframework.context.annotation.Bean
 import java.util.*
 
 @SpringBootApplication
@@ -16,6 +19,14 @@ class KioSchoolApiApplication {
     @PostConstruct
     fun started() {
         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"))
+    }
+
+    @Bean
+    fun commandLineRunner(ctx: ApplicationContext): CommandLineRunner {
+        return CommandLineRunner {
+            println("!!! [STARTUP_CHECK] APPLICATION IS RUNNING NEW VERSION !!!")
+            println("!!! Active Profiles: ${Arrays.toString(ctx.environment.activeProfiles)} !!!")
+        }
     }
 }
 
