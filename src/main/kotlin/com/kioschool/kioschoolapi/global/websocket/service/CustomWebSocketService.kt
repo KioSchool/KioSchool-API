4d331a73253f47be4c8243f175a2824141955ca4
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2026-04-03 00:42:49 +0900

    Add async to webswocket service

--- src/main/kotlin/com/kioschool/kioschoolapi/global/websocket/service/CustomWebSocketService.kt
+++ src/main/kotlin/com/kioschool/kioschoolapi/global/websocket/service/CustomWebSocketService.kt
@@ -2,12 +2,14 @@ package com.kioschool.kioschoolapi.global.websocket.service
 
 import com.kioschool.kioschoolapi.global.websocket.dto.Message
 import com.kioschool.kioschoolapi.global.websocket.redis.RedisPublisher
+import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Service
 
 @Service
 class CustomWebSocketService(
     private val redisPublisher: RedisPublisher
 ) {
+    @Async
     fun sendMessage(destination: String, message: Message) {
         redisPublisher.publish(destination, message)
     }
