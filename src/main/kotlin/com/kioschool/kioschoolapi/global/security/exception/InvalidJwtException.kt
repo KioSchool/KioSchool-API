cbb4a3f7a6590e022c55cff59f461aa4cea55bac
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2026-04-24 17:08:35 +0900

    Refactor 403 responses to 401 code in user and global exceptions

--- src/main/kotlin/com/kioschool/kioschoolapi/global/security/exception/InvalidJwtException.kt
+++ src/main/kotlin/com/kioschool/kioschoolapi/global/security/exception/InvalidJwtException.kt
@@ -3,5 +3,5 @@ package com.kioschool.kioschoolapi.global.security.exception
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
 
-@ResponseStatus(code = HttpStatus.FORBIDDEN, reason = "유효하지 않은 토큰입니다.")
+@ResponseStatus(code = HttpStatus.UNAUTHORIZED, reason = "유효하지 않은 토큰입니다.")
 class InvalidJwtException : Exception()
