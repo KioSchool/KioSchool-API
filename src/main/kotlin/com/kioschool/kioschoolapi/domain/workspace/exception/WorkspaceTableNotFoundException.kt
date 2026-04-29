c7bec8d7dc125c792a21afa88fe24fed84fa9b9a
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2026-04-30 00:18:13 +0900

    Fix typo

--- src/main/kotlin/com/kioschool/kioschoolapi/domain/workspace/exception/WorkspaceTableNotFoundException.kt
+++ src/main/kotlin/com/kioschool/kioschoolapi/domain/workspace/exception/WorkspaceTableNotFoundException.kt
@@ -3,5 +3,5 @@ package com.kioschool.kioschoolapi.domain.workspace.exception
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
 
-@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "존재하지 않는 태이블입니다.")
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "존재하지 않는 테이블입니다.")
 class WorkspaceTableNotFoundException : Exception()
